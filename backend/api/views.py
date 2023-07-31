034bf8a6cf2b1c16f325e67e278bcf2ab9343922
Горелько Никита <nikita.gorelko@mail.ru>
2023-07-31 22:25:41 +0300

    try subscriptions fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -186,7 +186,7 @@ class SubscribeView(APIView):
 class ShowSubscriptionsView(ListAPIView):
     """Вью-класс для отображения подписок."""
 
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
     def get(self, request):
         user = request.user
